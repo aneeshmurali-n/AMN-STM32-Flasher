01b2fdb7a85e727f78a895b1dc84177ab3fa3fe8
Aneesh Murali Nariyampully <54899074+aneeshmurali-n@users.noreply.github.com>
2025-10-04 16:22:42 +0530

    AMN STM32 Flasher.py

--- AMN STM32 Flasher.py
+++ AMN STM32 Flasher.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import os,sys
+import os
 import customtkinter as ctk
 import subprocess
 import time
@@ -390,3 +390,4 @@ class FlasherApp(ctk.CTk):
 if __name__ == "__main__":
     app = FlasherApp()
     app.mainloop()
+
