6b344c7e54a4803413ce781aef88c56f13528990
Aneesh Murali Nariyampully <54899074+aneeshmurali-n@users.noreply.github.com>
2025-10-25 20:01:19 +0530

    Update header comment and maintain functionality

--- AMN STM32 Flasher.py
+++ AMN STM32 Flasher.py
@@ -1,4 +1,4 @@
-# AMN STM32 Flasher
+# AMN STM32 Flasher - The world’s first flashing app that makes flashing as easy as moving a file.
 # Copyright 2025 Aneesh Murali Nariyampully
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -455,3 +455,4 @@ class FlasherApp(ctk.CTk):
 if __name__ == "__main__":
     app = FlasherApp()
     app.mainloop()
+
